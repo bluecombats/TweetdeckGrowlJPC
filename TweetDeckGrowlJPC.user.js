7de6381dab3c76257c929dc10c091fcd6c883f9f
James Clare <bluecombats@users.noreply.github.com>
2018-10-28 10:51:28 +0000

    Update 2.1 bugs got rid, and shorten the script

--- TweetDeckGrowlJPC.user.js
+++ TweetDeckGrowlJPC.user.js
@@ -1,164 +1,155 @@
 // ==UserScript==
-// @name          TweetDeck Growl JPC
-// @namespace     http://www.bluecombats.blogspot.com
-// @description	  Sends notifications from the tweetdeck website when there is a new tweet, Number of columns gets registered with growl app
-// @include       https://tweetdeck.twitter.com/*
-// @version        2.0
-//@date 2014-04-07
+// @name 			TweetDeck Growl JPC
+// @namespace		http://www.bluecombats.blogspot.com
+// @description		Sends notifications from the tweetdeck website when there is a new tweet in any column
+// @include			https://tweetdeck.twitter.com/*
+// @version			2.1
+// @date			2018-10-28
+// @run-at			document-end
 // ==/UserScript==
 
-	try{
-		function TweetdeckLoginScreen(){
-			var moveon="no";
-			while (moveon=="no"){
-				//test
-				console.log("nope");
-				if(!document.getElementsByClassName('js-app-loading')[0].getElementsByClassName('js-startflow-chrome app-masthead')[0]){
-					console.log("moving on");
-					moveon="yes";
-				}
-			}
+function TweetDeckColumns(){
+	var Var="exist";
+	var i=0;
+	while (Var=="exist"){	
+		if(document.getElementsByTagName("section")[i]){
+			i+=1;
+			console.log("Column "+i+" exists");
 		}
-		function TweetDeckColumns(){
-			var Var="exist";
-			var i=0;
-			while (Var=="exist"){	
-				if(document.getElementsByTagName("section")[i]){
-					i+=1;
-					console.log("Column "+i+" exists");
-				}
-				else{
-					Var="doesn't exist";
-				}	
-			}
-			columns=i;
-			return [columns];
-		}
-		function TweetDeckGrowlinit(appname,Columns){
-			console.log('Starting TweetDeck Growl');
-			
-			var j;
-			var ntNewTweet = {};
-			var types=new Array();
-			for(var i=1;i<=Columns;i++){
-				j=i-1;
-				ntNewTweet["Column"+i]={};
-				ntNewTweet["Column"+i].name='column'+i+'NewTweet';
-				ntNewTweet["Column"+i].displayName = 'Column '+i+' New Tweet';
-				ntNewTweet["Column"+i].enabled = true;
+		else{
+			Var="doesn't exist";
+		}	
+	}
+	columns=i;
+	return [columns];
+}
+/*function TweetDeckGrowlinit(appname,Columns){
+	console.log('Starting TweetDeck Growl');
+	
+	var j;
+	var ntNewTweet = {};
+	var types=new Array();
+	for(var i=1;i<=Columns;i++){
+		j=i-1;
+		ntNewTweet["Column"+i]={};
+		ntNewTweet["Column"+i].name='column'+i+'NewTweet';
+		ntNewTweet["Column"+i].displayName = 'Column '+i+' New Tweet';
+		ntNewTweet["Column"+i].enabled = true;
 
-				types [j]= ntNewTweet["Column"+i];
-				//console.log("registering column"+i);
-			}
-			//console.log(types);
-		}
-		function defaultValues(oldtweet,tweet,Columns){
-			var j;
-			for(var i=1;i<=Columns;i++){
-				j=i-1;
-				oldtweet[j]="Dinosaur walks into a bar";
-				tweet[j]="Dinosaur walks into a bar";
-			}
-			return [oldtweet,tweet];
-		}
-		function TweetDeckinterval(oldtweet,tweet,appname,Columns){
-			var j,i;
-			var column,TweetContainer, article, user;
-			var ColumnHeader,ColumnHeaderAccount,account=null,image=null,retweeter="";
-			// multiple columns
-			for(i=1;i<=Columns;i++){
-  				j=i-1;
-				account=null;
-				retweeter="";
-				
-				console.log('Column '+i);
-				oldtweet[j]=tweet[j];
-				//default
-				tweet[j]=null;
-				
-				//columns
-				column=document.getElementsByTagName('section')[i];
-				//console.log('column found');			   
-				article=column.getElementsByTagName('article')[0];
-				user=article.getElementsByClassName("nbfc")[0].innerHTML;
-				tweet[j]=article.getElementsByTagName("p")[0].innerHTML;
-				image=article.getElementsByTagName("img")[0].src;
-				
-				user=removeHtml(user);
-				tweet[j]=removeHtml(tweet[j]);
-				
-				if(oldtweet[j]!=tweet[j]){
-					console.log("column"+i+"NewTweet");
-					var notification=new Notification(creator,{
-						icon: image,
-						body: tweet[j],
-					});
-				}
-				else{
-					console.log('no new tweet');
-				}
-			}
-			return [oldtweet,tweet];
-		}
-		function removeHtml(string){
-			//find 1st occurence of <
-			var lessthan=string.indexOf("<");
-			while(lessthan!=-1){
-				//console.log("check: "+tweet);
-				//find 1st occurence of >
-				var greaterthan=string.indexOf(">");
-				//the html stuff
-				var htmlstuff=string.substring(lessthan,greaterthan+1);
-				//replacing html with nothing
-				string=string.replace(htmlstuff,"");
-				//update lessthan
-				lessthan=string.indexOf("<");
-			}
-			//console.log("end of if statements");
-			return string;
-		}
-		function removeHtmlEntities(string){
-			var entitiesNumbers=["&#34;","&#39;","&#38;","&#60;","&#62;","&#160;","&#161;","&#162;","&#163;","&#164;",
-			"&#165;","&#166;","&#167;","&#168;","&#169;","&#170;","&#171;","&#172;","&#173;","&#174;",
-			"&#175;","&#176;","&#177;","&#178;","&#179;","&#180;","&#181;","&#182;","&#183;","&#184;",
-			"&#185;","&#186;","&#187;","&#188;","&#189;","&#190;","&#191;","&#215;","&#247;"];
-			var entitiesName=["&quot;","&apos;","&amp;","&lt;","&gt;","&nbsp;","&iexcl;","&cent;","&pound;","&curren;",
-			"&yen;","&brvbar;","&sect;","&uml;","&copy;","&ordf;","&laquo;","&not;","&shy;","&reg;",
-			"&macr;","&deg;","&plusmn;","&sup2;","&sup3;","&acute;","&micro;","&para;","&middot;","&cedil;",
-			"&sup1;","&ordm;","&raquo;","&frac14;","&frac12;","&frac34;","&iquest;","&times;","&divide;"];
-			var character=["\"","'","&","<",">"," ","¡","¢","£","¤",
-			"¥","¦","§","¨","©","ª","«","¬","?­","®",
-			"¯","°","±","²","³","´","µ","¶","·","¸",
-			"¹","º","»","¼","½","¾","¿","×","÷"];
-			var replace="yes";
-			while(replace=="yes"){
-				//replace
-				for(var i=0;i<entitiesNumbers.length;i++){
-					var Number=string.indexOf(entitiesNumbers[i]);
-					var Name=string.indexOf(entitiesName[i]);
-					if(Number!=-1){
-						string=string.replace(entitiesNumbers[i],character[i]);
-					}
-					if(Name!=-1){
-						string=string.replace(entitiesName[i],character[i]);
-					}
-				}
-				replace="no";
-				//check if there is more replacements
-				for(var i=0;i<entitiesNumbers.length;i++){
-					var Number=string.indexOf(entitiesNumbers[i]);
-					var Name=string.indexOf(entitiesName[i]);
-					if(Number!=-1){
-						replace="yes";
-					}
-					if(Name!=-1){
-						replace="yes";
-					}
-				}
-			}
-			return string;
+		types [j]= ntNewTweet["Column"+i];
+		//console.log("registering column"+i);
+	}
+	//console.log(types);
+}*/
+function defaultValues(oldtweet,tweet,Columns){
+	var j;
+	for(var i=0;i<=Columns;i++){
+		j=i;
+		oldtweet[j]="Dinosaur walks into a bar";
+		tweet[j]="Dinosaur walks into a bar";
+	}
+	return [oldtweet,tweet];
+}
+function TweetDeckinterval(oldtweet,tweet,Columns){
+	var j,i;
+	var column,TweetContainer, article, user;
+	var ColumnHeader,ColumnHeaderAccount,account=null,image=null,retweeter="";
+	// multiple columns
+	for(i=1;i<Columns;i++){
+		j=i-1;
+		account=null;
+		retweeter="";
+		
+		console.log('Column '+i);
+		oldtweet[j]=tweet[j];
+		//default
+		tweet[j]=null;
+		
+		//columns
+		column=document.getElementsByTagName('section')[i];
+		//console.log('column found');
+		if(column.getElementsByTagName('article')[0]){
+			article=column.getElementsByTagName('article')[0];
+			user=article.getElementsByClassName("nbfc")[0].innerHTML;
+			tweet[j]=article.getElementsByTagName("p")[0].innerHTML;
+			image=article.getElementsByTagName("img")[0].src;
+		
+			user=removeHtml(user);
+			tweet[j]=removeHtml(tweet[j]);
 		}
 		
+		if(oldtweet[j]!=tweet[j]){
+			console.log("column"+i+"NewTweet");
+			var notification=new Notification(user,{
+				icon: image,
+				body: tweet[j],
+			});
+		}
+		else{
+			console.log('no new tweet');
+		}
+	}
+	return [oldtweet,tweet];
+}
+function removeHtml(string){
+	//find 1st occurence of <
+	var lessthan=string.indexOf("<");
+	while(lessthan!=-1){
+		//console.log("check: "+tweet);
+		//find 1st occurence of >
+		var greaterthan=string.indexOf(">");
+		//the html stuff
+		var htmlstuff=string.substring(lessthan,greaterthan+1);
+		//replacing html with nothing
+		string=string.replace(htmlstuff,"");
+		//update lessthan
+		lessthan=string.indexOf("<");
+	}
+	//console.log("end of if statements");
+	return string;
+}
+function removeHtmlEntities(string){
+	var entitiesNumbers=["&#34;","&#39;","&#38;","&#60;","&#62;","&#160;","&#161;","&#162;","&#163;","&#164;",
+	"&#165;","&#166;","&#167;","&#168;","&#169;","&#170;","&#171;","&#172;","&#173;","&#174;",
+	"&#175;","&#176;","&#177;","&#178;","&#179;","&#180;","&#181;","&#182;","&#183;","&#184;",
+	"&#185;","&#186;","&#187;","&#188;","&#189;","&#190;","&#191;","&#215;","&#247;"];
+	var entitiesName=["&quot;","&apos;","&amp;","&lt;","&gt;","&nbsp;","&iexcl;","&cent;","&pound;","&curren;",
+	"&yen;","&brvbar;","&sect;","&uml;","&copy;","&ordf;","&laquo;","&not;","&shy;","&reg;",
+	"&macr;","&deg;","&plusmn;","&sup2;","&sup3;","&acute;","&micro;","&para;","&middot;","&cedil;",
+	"&sup1;","&ordm;","&raquo;","&frac14;","&frac12;","&frac34;","&iquest;","&times;","&divide;"];
+	var character=["\"","'","&","<",">"," ","¡","¢","£","¤",
+	"¥","¦","§","¨","©","ª","«","¬","?­","®",
+	"¯","°","±","²","³","´","µ","¶","·","¸",
+	"¹","º","»","¼","½","¾","¿","×","÷"];
+	var replace="yes";
+	while(replace=="yes"){
+		//replace
+		for(var i=0;i<entitiesNumbers.length;i++){
+			var Number=string.indexOf(entitiesNumbers[i]);
+			var Name=string.indexOf(entitiesName[i]);
+			if(Number!=-1){
+				string=string.replace(entitiesNumbers[i],character[i]);
+			}
+			if(Name!=-1){
+				string=string.replace(entitiesName[i],character[i]);
+			}
+		}
+		replace="no";
+		//check if there is more replacements
+		for(var i=0;i<entitiesNumbers.length;i++){
+			var Number=string.indexOf(entitiesNumbers[i]);
+			var Name=string.indexOf(entitiesName[i]);
+			if(Number!=-1){
+				replace="yes";
+			}
+			if(Name!=-1){
+				replace="yes";
+			}
+		}
+	}
+	return string;
+}
+
 //Main Script starts here
 console.log("Starting TweetDeck Growl JPC")
 Notification.requestPermission().then(function(result) {
@@ -172,9 +163,7 @@ Notification.requestPermission().then(function(result) {
 	}
 	setTimeout(function(){
 		var GroupVar,Columns,k,once="no",second="no";
-		var appname= 'TweetDeck Growl';
-		//find out if it's on the login screen
-		//TweetdeckLoginScreen();
+		//var appname= 'TweetDeck Growl';
 		var variable=setInterval(function(){
 			if(!document.getElementsByClassName('js-app-loading')[0].getElementsByClassName('js-startflow-chrome app-masthead')[0]){
 				//clearInterval(variable);
@@ -185,14 +174,14 @@ Notification.requestPermission().then(function(result) {
 					//find out how many columns there are
 					GroupVar=TweetDeckColumns();
 					Columns=GroupVar[0];
-					TweetDeckGrowlinit(appname,Columns);
+					//TweetDeckGrowlinit(appname,Columns);
 					tweet={};
 					oldtweet={};
 					var Myvar=defaultValues(oldtweet,tweet,Columns);
 					oldtweet=Myvar[0];
 					tweet=Myvar[1];
 				}
-				Myvar=TweetDeckinterval(oldtweet,tweet,appname,Columns);
+				Myvar=TweetDeckinterval(oldtweet,tweet,Columns);
 				oldtweet=Myvar[0];
 				tweet=Myvar[1];
 			}
@@ -200,28 +189,6 @@ Notification.requestPermission().then(function(result) {
 				console.log("still on log on screen");
 			}
 		},1000);
-        //find out how many columns there are
-        /*GroupVar=TweetDeckColumns();
-	Columns=GroupVar[0];
-	k=GroupVar[1];
-	TweetDeckGrowlinit(appname,Columns);
-	tweet={};
-	oldtweet={};
-	var Myvar=defaultValues(oldtweet,tweet,Columns);
-	oldtweet=Myvar[0];
-	tweet=Myvar[1];
-	setInterval(function(){
-		Myvar=TweetDeckinterval(oldtweet,tweet,appname,Columns,k);
-		oldtweet=Myvar[0];
-		tweet=Myvar[1];
-	},2000);*/            
 	},10000);
-}
-    catch(err){
-        txt="There was an error on this page.\n";   
-        txt+="Error description: " + err.message + "\n"; 
-        txt+="Error line "+err.lineNumber+ "\n";
-        txt+="Click OK to continue.\n";   
-        console.log(txt);
-    }
-    console.log("end of loop");
+})
+console.log("end of loop");
